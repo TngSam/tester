41199342e35cb0aadbbe359f22116025dd47a2b3
TngSam <ash_dj@mail.ru>
2019-01-25 16:20:45 +0300

    :building_construction: New action types and action constructors

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -1,4 +1,5 @@
 import createAsyncAction from './createAsyncAction';
 
 export const LOGIN = createAsyncAction('LOGIN');
+export const REGISTER = createAsyncAction('REGISTER');
 export const LOGOUT = 'LOGOUT';
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,15 +1,29 @@
-import { LOGIN } from './actionTypes';
+import { LOGIN, REGISTER } from './actionTypes';
 
-export const loginRequest = (payload) => ({
+export const loginRequest = payload => ({
   type: LOGIN.REQUEST,
   payload
 });
-export const loginError = (payload) => ({
+export const loginError = payload => ({
   type: LOGIN.ERROR,
   payload,
   error: true
 });
-export const loginSuccess = (payload) => ({
+export const loginSuccess = payload => ({
   type: LOGIN.SUCCESS,
+  payload
+});
+
+export const registerRequest = payload => ({
+  type: REGISTER.REQUEST,
+  payload
+});
+export const registerError = payload => ({
+  type: REGISTER.ERROR,
+  payload,
+  error: true
+});
+export const registerSuccess = payload => ({
+  type: REGISTER.SUCCESS,
   payload
 });
